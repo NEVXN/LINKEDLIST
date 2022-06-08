60523363891e45d7ed4c121b03b1e8ef9c54b964
NEVIN TOM <94775838+NEVXN@users.noreply.github.com>
2022-06-08 14:47:19 +0530

    Add files via upload

--- stack1.c
+++ stack1.c
@@ -1,68 +1,130 @@
-#include<stdio.h>
-#include<stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
+#define n 5 
+int top=-1;
+int stack[n];
+
+//checking if stack is full 
+int isful
+{
+	if(top==-1)
+	{
+		return 1;
+	}
+	else
+	{
+		return 0;
+	}
+}
+
+//pushing the element                                                                                                                                                                        
+void push(int val)
+{
+	if (isfull())
+	{
+		printf("\nOverflow");
+	}
+	else
+	{
+		top++;
+		stack[top]=val;
+		printf("\n%d ELEMENT PUSHED\n",val);
+	}	
+}
+//popping the element
+void pop()
+{
+	if (isempty())
+	{
+		printf("\nUnderflow");
+	}
+	else
+	{
+		printf("\n%d POPPED",stack[top]);
+		stack[top]=-1;
+		top--;
+	}
+}
 void main()
 {
-	struct node
+	for(int i=0;i<n;i++)
 	{
-		struct node* prev;
-		int data;
-		struct node *next;
-	};
-	int ch,entry;
-	struct node *head=NULL,*pos=NULL, *tail=NULL;
+		stack[i]=-1;
+	}
+	printf("PROGRAM TO PERFORM OPERAIONS ON STACK");
 	while(1)
 	{
-	printf("\n\t\tMenu\n1.Append\n2.Display(Forward)\n3.Display(Reverse)\n4.Exit\n\nEnter choice: ");
-	scanf("%d",&ch);
-	switch(ch)
-	{
-		case 1:
+		printf("\n1.Push\n2.Pop\n3.Display\n4.Exit\n");
+		printf("Enter your choice");
+		int ch;
+		scanf("%d",&ch);
+		switch(ch)
 		{
-			printf("Enter data: ");
-			scanf("%d",&entry);
-			if(head==NULL)
+			case 1:
 			{
-				head=(struct node*)malloc(sizeof(struct node));
-				head->prev=NULL;
-				head->data= entry;
-				head->next=NULL;
-				pos=head;
-				tail=head;
+				int num;
+				printf("Enter value to be pushed");
+				scanf("%d",&num);
+				push(num);
+				break;
 			}
-			else
+			case 2:
 			{
-			tail->next=(struct node *)malloc(sizeof(struct node));
-			tail->next->prev=tail;
-			tail->next->data=entry;
-			tail->next->next=NULL;
-			tail=tail->next;
-			} break;
-		}
-		case 2:
-		{	
-			pos=head;
-			printf("Elements are: ");
-			while(pos!=NULL)
+				pop();
+				break;
+			}
+			case 3:
 			{
-			printf("\t%d",pos->data);
-			pos=pos->next;
-			} break;
-		}
-		case 3:
-		{
-			pos = tail;
-			printf("elements are: ");
-			while(pos!=NULL)
+				int i=top;
+				if (isempty())
+				{
+				printf("No elements to display. Insert more elements");
+				}
+				else
+				{
+				while(i>=0)
+				{
+					if(i==top)
+					{
+						printf("%d--->top\n",stack[top]);
+						i--;
+					}
+					else
+					{
+						printf("%d\n",stack[i]);
+						i--;
+					}
+				}
+				for(i=0;i<n;i++)
+				{
+					printf("%d\t",stack[i]);
+				}
+				}
+				break;
+			}
+			default:
 			{
-			printf("\t%d",pos->data);
-			pos=pos-> prev;
-			} break;
+				printf("invalid option");
+				break;
+			}
+			case 4:
+			{
+				printf("Exiting Program...\n");
+				exit(0);
+			}
 		}
-		case 4:
-		{
-			printf("Goodbye!");
-			exit(0);
-		}
+	}	
+}l()
+{
+	if(top==n-1)
+	{
+		return 1;
 	}
+	else
+	{
+		return 0;
 	}
-	}
+}
+
+//checking if stack is empty
+int isempty()
